1d4aa7a532eb9187102fe4254047d4b91876d059
Lucas Somacal <lsomacal@gmail.com>
2022-02-18 22:55:06 -0300

    Tested train_model

--- pipeline_tests/test_model.py
+++ pipeline_tests/test_model.py
@@ -1,8 +1,9 @@
 import keras.models
 import pytest
 
-from model.train import train_new_model
+from model.train import train_new_model, train_model
 from utils.files_utils import load_pickle
+from utils.plots_utils import plot_train
 
 
 @pytest.fixture
@@ -37,20 +38,18 @@ def test_new_model_1_epoch_pkl():
         assert len(l) == epochs + 1
 
 
-def test_train_model(sonata15_mapleleaf_prep):
-    ...
+def test_train_model():
+    epochs = 5
+    model, callbacks = train_model("mapleleaf_ds", "test_new_model", epochs, ckpt=2)
+    assert callbacks != {}
+    assert set(callbacks.keys()) == {"decoder_loss", "decoder_1_loss", "decoder_2_loss", "decoder_3_loss", "loss"}
+    for l in callbacks.values():
+        assert len(l) == epochs + 1 - 2
+    return callbacks
 
 
 def test_plot_visualization():
-    ...
+    final_epoch = 2 + 5 + 5
+    callbacks = test_train_model()
+    plot_train(callbacks, final_epoch)
 
-
-'''
-from model.train import train_model
-
-# rolls_preprocessed = np.stack([r.roll for r in df['roll']])
-# vae, callbacks = train_model(rolls_preprocessed, if_train, entrenar_nuevo, epoca_final)
-vae, callbacks = train_model(df_midi, if_train, entrenar_nuevo, epoca_final)
-
-plot_train(callbacks, epoca_final)
-'''
