298cba13527321b676f4ac3a9af8ee1545c21be4
Lucas Somacal <lsomacal@gmail.com>
2023-03-29 15:52:17 -0300

    Resúmenes de evaluación general

--- evaluation/overall_evaluation.py
+++ evaluation/overall_evaluation.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import seaborn as sns
 
 from utils.plots_utils import save_plot
@@ -19,10 +20,25 @@ def overall_evaluation(metrics, plot_dir, context='talk'):
     for orig, df in metrics["Style"].items():
         heatmap_style_evaluation(df, orig, plot_dir, context)
 
+    d = {orig: [(s, df[s][s]) for s in df.columns] for orig, df in metrics["Style"].items()}
+    df_style = pd.DataFrame(index=metrics["Style"].keys(), columns=metrics["Style"].keys())
+    for s in d.keys():
+        df_style.at[s, s] = 0
+    for orig, l in d.items():
+        for dest, value in l:
+            df_style.at[orig, dest] = value  # index: orig; columns: dest
+    df_style.to_csv(plot_dir + "/overall_style.csv", index=True)
+
     # Plot heatmap musicality
     sns.heatmap(metrics["Musicality"], annot=True, fmt='g', vmin=0, vmax=100)
     save_plot(plot_dir, "heatmap_musicality", "Avg of % of permutations\nthat are less musical than the generated")
+    d = {s: [sum(metrics["Musicality"][s])] for s in metrics["Musicality"].columns}
+    df_mus = pd.DataFrame(d)
+    df_mus.to_csv(plot_dir + "/overall_musicality.csv", index=False)
 
     # Plot heatmap plagiarism
     sns.heatmap(metrics["Plagiarism"], annot=True, fmt='g', vmin=0, vmax=1)
     save_plot(plot_dir, "heatmap_plagiarism", "Avg of % of rolls\nthat are more different than the generated")
+    d = {s: [sum(metrics["Plagiarism"][s])] for s in metrics["Plagiarism"].columns}
+    df_plagiarism = pd.DataFrame(d)
+    df_plagiarism.to_csv(plot_dir + "/overall_plagiarism.csv", index=False)
