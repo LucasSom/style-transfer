d07ff359329551a7ecc31d2d79739ca08172d29e
Lucas Somacal <lsomacal@gmail.com>
2023-07-29 18:21:52 -0300

    Embeddings tests amended

--- pipeline_tests/test_embeddings.py
+++ pipeline_tests/test_embeddings.py
@@ -7,7 +7,7 @@ from dodo import analyze_training, do_embeddings, models
 from model.colab_tension_vae.params import init
 from model.embeddings.embeddings import obtain_embeddings
 from model.embeddings.style import Style
-from utils.files_utils import load_pickle, preprocessed_data_dir, path_saved_models, get_embedding_path, \
+from utils.files_utils import load_pickle, preprocessed_data_dir, get_embedding_path, \
     get_reconstruction_path, get_characteristics_path, get_emb_path, data_path, get_model_paths, preprocessed_data_path
 from utils.plots_utils import plot_characteristics_distributions
 
@@ -15,7 +15,6 @@ from utils.plots_utils import plot_characteristics_distributions
 @pytest.fixture
 def brmf4_prep():
     return load_pickle(file_name=preprocessed_data_dir + "bach-rag-moz-fres-4")
-
 
 
 @pytest.fixture
@@ -27,6 +26,7 @@ def brmf4_emb():
 def model_name():
     return "bach_rag_moz_fres"
 
+
 @pytest.fixture
 def characteristics():
     init(4)
@@ -36,7 +36,7 @@ def characteristics():
         "Sigma": [np.zeros(96), np.zeros(96), np.ones(96)],
     })
 
-    df = load_pickle(f'{data_path}4-small_br/embeddings/df_emb.pkl')
+    df = load_pickle(f'{data_path}models/4-small_br/embeddings/df_emb.pkl')
     df["Style"][:2] = "Pepe"
     df["Style"][2:4] = "Jose"
     df["Style"][4:] = "Baile"
@@ -47,18 +47,18 @@ def characteristics():
         "Baile": Style("Baile", df_char=df_char, df=df)
     }
 
-    styles["Pepe"].intervals_distribution = np.ones((24,24))
-    styles["Jose"].intervals_distribution = np.ones((24,24))
-    styles["Jose"].intervals_distribution[10,11] += 1
+    styles["Pepe"].intervals_distribution = np.ones((24, 24))
+    styles["Jose"].intervals_distribution = np.ones((24, 24))
+    styles["Jose"].intervals_distribution[10, 11] += 1
 
-    styles["Baile"].intervals_distribution = np.zeros((24,24))
-    styles["Baile"].intervals_distribution[10:13,10:13] += 2
+    styles["Baile"].intervals_distribution = np.zeros((24, 24))
+    styles["Baile"].intervals_distribution[10:13, 10:13] += 2
 
     return styles
 
 
 def test_obtain_embeddings(brmf4_prep):
-    vae = keras.models.load_model(path_saved_models)
+    vae = keras.models.load_model(get_model_paths("brmf_4b")[1])
 
     df_emb = obtain_embeddings(brmf4_prep, vae)
 
@@ -68,29 +68,29 @@ def test_obtain_embeddings(brmf4_prep):
 
 
 def test_analyze_training():
-    b = 4
+    b, z = 4, 96
     model_name = 'brmf_4b_beta'
     val_path = f"{preprocessed_data_dir}{b}val.pkl"
     analyze_training(train_path=preprocessed_data_path(4, False), val_path=val_path, model_name=model_name, b=4,
-                     targets=get_reconstruction_path(model_name))
+                     targets=[get_reconstruction_path(model_name)], z=z)
 
 
-def test_plot_distributions(characteristics):
-    plot_dir = data_path + 'debug_outputs/'
-    plot_name = "distributions"
-    plot_characteristics_distributions(characteristics, plot_dir, plot_name)
+# def test_plot_distributions(characteristics):
+#     plot_dir = data_path + 'debug_outputs/'
+#     plot_name = "distributions"
+#     plot_characteristics_distributions(characteristics, plot_dir, plot_name)
 
 
 def test_characteristics():
     model_name = '4-small_br'
-    model_path = data_path + f'/{model_name}/vae'
+    model_path = data_path + f'models/{model_name}/vae'
     do_embeddings(preprocessed_data_path(4, False), model_path, model_path, get_characteristics_path(model_name),
-                  get_emb_path(model_name), 4)
+                  get_emb_path(model_name), 4, z=96)
 
 
 def test_characteristics_beta():
     model_name = 'brmf_4b_beta'
-    b = 4
+    b, z = 4, 96
 
     model_path, vae_dir, vae_path = get_model_paths(model_name)
     characteristics_path = get_characteristics_path(model_name)
@@ -101,10 +101,11 @@ def test_characteristics_beta():
                   model_path,
                   vae_dir,
                   characteristics_path,
-                  emb_path, b)
+                  emb_path, b, z)
 
-def test_all_models_characteristics():
-    for model_name in models:
-        model_path = data_path + f'/{model_name}/vae'
-        do_embeddings(preprocessed_data_path(4, False), model_path, model_path, get_characteristics_path(model_name),
-                      get_emb_path(model_name), 4)
+
+# def test_all_models_characteristics():
+#     for model_name in models:
+#         model_path = data_path + f'models/{model_name}/vae'
+#         do_embeddings(preprocessed_data_path(4, False), model_path, model_path, get_characteristics_path(model_name),
+#                       get_emb_path(model_name), 4, z=96)
--- utils/files_utils.py
+++ utils/files_utils.py
@@ -12,7 +12,6 @@ datasets_path = os.path.join(data_path, 'datasets')
 datasets_debug_path = os.path.join(datasets_path, 'debug')
 data_tests_path = data_path + 'tests/'
 preprocessed_data_dir = data_path + 'preprocessed_data/'
-path_saved_models = data_path + 'brmf_4b/vae/'
 original_audios_path = os.path.join(preprocessed_data_dir, 'original/audios/')
 
 
